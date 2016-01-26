a4c51434803d1cd228ced3179508085322681db2
arturalkaim <arturalkaim@tecnico.ulisboa.pt>
2016-01-26 14:30:03 +0000

    Test file update
    Commiting all files to make large merge

--- racket/surfacesTest.rkt
+++ racket/surfacesTest.rkt
@@ -9,11 +9,12 @@
     )
 (let* ([p0 (xyz 0.0 0.0 0.0)]
        [p1 (+y p0 5.0)]
-       [p2 (+xy p0 2.1 5.0)]
-       [p3 (+xy p0 -2.1 5.0)])
+       [p2 (+xy p1 2.1 5.0)]
+       [p3 (+xy p1 -2.1 5.0)])
   ;  (reg-surface p0 7.0 2.0 2.0 p1)
-  
-  (reg-line p0 7.0 4.0 4.0 p1 0.0 0.0 0.0 1.0)
+  ;(polygon-surface (list p0 p1 p2 p3) 0.0 1.0 1.0)
+  (line (list p0 p1 p2 p3) 0.0 0.0 1.0)
+  (displayln (reg-line p0 7.0 4.0 4.0 p1 0.0 0.0 0.0 1.0))
   (reg-line p0 7.0 4.0 4.0 p2 90 1.0 0.0 0.0)
   (reg-surface p0 7.0 4.0 4.0 p3 pi/2 0.0 1.0 0.0)
   )
@@ -23,14 +24,14 @@
          [p1 (+z p0 15.0)])
     (trunk p0 5.0 5.0 17.0 17.0 p1 5.0))
 
-(let* ([p0 (xyz 0.0 5.0 0.0)]
+#;(let* ([p0 (xyz 0.0 5.0 0.0)]
        [p1 (+yz p0 0.0 20.0)]
        [a1 (/ pi 2)]
        [a2 (* (/ pi 6) 7)]
        [a3 (- (/ pi 3))])
-  (irregularPyramid p0 p1 35.0 a1 37.0 a2 37.0 a3 0.0 0.7 0.0))
+  (irregularPyramid3 p0 p1 35.0 a1 37.0 a2 37.0 a3 0.0 0.7 0.0))
 
-(point (u0) 1.0 0.0 0.0)
+;(point (u0) 1.0 1.0 0.0)
 
 (time
  (begin
